a8e1b12bd71e190493e93a04051dd3afb51c348e
stvelikiy-star <st.velikiy@gmail.com>
2026-08-27 09:36:36 +0600

    feat(pms): compose guarded bulk ops into V9

--- apps/admin/components/PMSGridV9.tsx
+++ apps/admin/components/PMSGridV9.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import PMSBulkGuardV9 from "./PMSBulkGuardV9";
 import PMSOperationsCockpitV9 from "./PMSOperationsCockpitV9";
 import PMSGridV8 from "./PMSGridV8";
 
 export default function PMSGridV9() {
   return <div className="pms-v9-stack">
     <PMSOperationsCockpitV9 />
+    <PMSBulkGuardV9 />
     <PMSGridV8 />
   </div>;
 }
