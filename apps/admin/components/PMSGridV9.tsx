efd503cdd5cf847d0cbaf1be9197c3b87f1a3c26
stvelikiy-star <st.velikiy@gmail.com>
2026-08-27 09:40:07 +0600

    refactor(pms): compose V9 directly on universal board

--- apps/admin/components/PMSGridV9.tsx
+++ apps/admin/components/PMSGridV9.tsx
@@ -2,12 +2,12 @@
 
 import PMSBulkGuardV9 from "./PMSBulkGuardV9";
 import PMSOperationsCockpitV9 from "./PMSOperationsCockpitV9";
-import PMSGridV8 from "./PMSGridV8";
+import PMSUniversalBoard from "./PMSUniversalBoard";
 
 export default function PMSGridV9() {
   return <div className="pms-v9-stack">
     <PMSOperationsCockpitV9 />
     <PMSBulkGuardV9 />
-    <PMSGridV8 />
+    <PMSUniversalBoard />
   </div>;
 }
