1eda8e8129e9ac15e50e274e0f7509f8acfbebba
stvelikiy-star <st.velikiy@gmail.com>
2026-08-27 08:31:55 +0600

    fix(pms): keep v8 active surface fail-closed

--- apps/admin/components/PMSGridV8.tsx
+++ apps/admin/components/PMSGridV8.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import PMSBulkOps from "./PMSBulkOps";
-import PMSReceptionCockpit from "./PMSReceptionCockpit";
-import PMSShiftControl from "./PMSShiftControl";
 import PMSUniversalBoard from "./PMSUniversalBoard";
 
 export default function PMSGridV8() {
   return <div className="pms-v8-stack">
-    <PMSShiftControl />
-    <PMSReceptionCockpit />
-    <PMSBulkOps />
     <PMSUniversalBoard />
   </div>;
 }
