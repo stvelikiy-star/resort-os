83c9f322cb3557d61e5f3fd46ce25fd2b70cd1c1
stvelikiy-star <st.velikiy@gmail.com>
2026-08-27 09:33:11 +0600

    feat(pms): activate integrated V9 chessboard

--- apps/admin/components/AdminShell.tsx
+++ apps/admin/components/AdminShell.tsx
@@ -5,7 +5,7 @@ import DashboardBoard from "./DashboardBoard";
 import HotelFinanceBoard from "./HotelFinanceBoard";
 import InboxBoard from "./InboxBoard";
 import OperationsBoard from "./OperationsBoard";
-import PMSGrid from "./PMSGridV8";
+import PMSGrid from "./PMSGridV9";
 import ReceptionBoard from "./ReceptionBoard";
 import RequestsBoard from "./RequestsBoard";
 import StaffBoard from "./StaffBoard";
