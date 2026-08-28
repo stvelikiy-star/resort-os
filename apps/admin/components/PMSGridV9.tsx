d3861e3a0b79b7d41b1e03f62c97443cd7dea724
stvelikiy-star <st.velikiy@gmail.com>
2026-08-28 21:31:59 +0600

    Wire V10 unified control into live PMS V9

--- apps/admin/components/PMSGridV9.tsx
+++ apps/admin/components/PMSGridV9.tsx
@@ -3,6 +3,7 @@
 import PMSBulkGuardV9 from "./PMSBulkGuardV9";
 import { PMSControlSnapshotProviderV9 } from "./PMSControlSnapshotV9";
 import PMSGuestServicesV9 from "./PMSGuestServicesV9";
+import PMSIntegrationRailV10 from "./PMSIntegrationRailV10";
 import PMSOperationsCockpitV9 from "./PMSOperationsCockpitV9";
 import PMSUniversalBoard from "./PMSUniversalBoard";
 
@@ -10,6 +11,7 @@ export default function PMSGridV9() {
   return (
     <PMSControlSnapshotProviderV9>
       <div className="pms-v9-stack">
+        <PMSIntegrationRailV10 />
         <PMSOperationsCockpitV9 />
         <PMSGuestServicesV9 />
         <PMSBulkGuardV9 />
