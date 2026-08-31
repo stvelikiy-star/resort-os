35e6dfebf8e662b9de920896e609bfd42fb9fbaf
stvelikiy-star <st.velikiy@gmail.com>
2026-09-01 00:44:57 +0600

    Show in-stay guest requests in staff PWA

--- apps/staff/app/page.tsx
+++ apps/staff/app/page.tsx
@@ -1,5 +1,9 @@
+import GuestRequestShiftPanel from "../components/GuestRequestShiftPanel";
 import StaffShiftV2 from "../components/StaffShiftV2";
 
 export default function StaffPage() {
-  return <StaffShiftV2 />;
+  return <>
+    <StaffShiftV2 />
+    <GuestRequestShiftPanel />
+  </>;
 }
