0acda127df8a411f1201c345de41b11b7c326b15
stvelikiy-star <st.velikiy@gmail.com>
2026-08-31 17:53:23 +0600

    feat(dining): expose staff dining route

---
+++ apps/staff/app/dining/page.tsx
@@ -0,0 +1,4 @@
+import DiningConsole from "../../components/DiningConsole";
+import "./dining.css";
+export const metadata={title:"Три Короны · Столовая"};
+export default function DiningPage(){return <DiningConsole/>}
