2dc500ed5ce4693acd472174fab7b47382ef931d
stvelikiy-star <st.velikiy@gmail.com>
2026-09-06 12:30:56 +0600

    feat(dining): compose restaurant folio routes

--- services/api/app/app_entry.py
+++ services/api/app/app_entry.py
@@ -9,6 +9,7 @@ from .crm_sync import router as crm_sync_router
 from .dining_control import router as dining_control_router
 from .dining_entitlements import router as dining_entitlements_router
 from .dining_floor_layout import router as dining_floor_layout_router
+from .dining_folio import router as dining_folio_router
 from .dining_production_snapshots import router as dining_production_snapshots_router
 from .dining_realtime import router as dining_realtime_router
 from .dining_seating import router as dining_seating_router
@@ -127,6 +128,7 @@ app.include_router(kitchen_admin_router)
 app.include_router(kitchen_arrivals_router)
 app.include_router(dining_control_router)
 app.include_router(dining_entitlements_router)
+app.include_router(dining_folio_router)
 app.include_router(dining_production_snapshots_router)
 app.include_router(dining_seating_router)
 app.include_router(dining_stays_router)
